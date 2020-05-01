adbbdb3e8733d7215083b8809c92a7e2baf1cf97
xooto <51514025+xooto@users.noreply.github.com>
2020-05-01 14:53:16 +0300

    Update urls.py

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^favicon\.ico$', RedirectView.as_view(url='static/favicon.ico'), name='favicon'),
     path('', views.index),
     path('infoVideo/', views.info),
     path('video/', views.creat),
